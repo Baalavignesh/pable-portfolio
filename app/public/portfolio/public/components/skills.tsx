df7957b4136a30bb38d88d455b3f930ca580ff1f
Baalavignesh <baalavignesh21@gmail.com>
2025-04-10 18:37:19 -0400

    Refactor skills data structure to include tags and colors; update Skills component to display skill names with corresponding colors

--- app/public/portfolio/public/components/skills.tsx
+++ app/public/portfolio/public/components/skills.tsx
@@ -6,15 +6,13 @@ const Skills: React.FC = () => {
     <div className="flex flex-col w-full  p-4 rounded-md border-gray-200 border-2 shadow-sm items-start h-fit mt-4">
       <Heading heading="Skills" linkto="skills"></Heading>
       <div className="flex flex-wrap gap-2 pt-2">
-      {
-        portlang.map((skill: string, index:number) => (
-            <div key={index} className="flex gap-2 items-center bg-gray-200 rounded-md p-2 py-1 text-black font-medium">
-                <p className="text-sm">{skill}</p>
+          {portlang.map((skill, index) => (
+            <div key={index} className="flex gap-2 items-center bg-gray-100/50 rounded px-3 py-1.5 text-gray-800 font-medium">
+                <div className={`w-2 h-2 rounded-full ${skill.color}`}></div>
+                <p className="text-sm">{skill.name}</p>
             </div>
-            ))
-      }
+          ))}
       </div>
-     
     </div>
   );
 };
--- app/public/portfolio/public/constants/skills.ts
+++ app/public/portfolio/public/constants/skills.ts
@@ -1,16 +1,64 @@
-let portlang: string[] = [
-    "TypeScript",
-    "JavaScript", 
-    "Python",
-    "Docker",
-    "Kubernetes",
-    "React",
-    "NextJs",
-    "Express", 
-    "AWS",
-    "Tailwind CSS",
-    "MySQL",
-    "DevOps"
-]
+let portlang: { name: string; tag: string; color: string }[] = [
+  {
+    name: "TypeScript",
+    tag: "frontend",
+    color: "bg-green-500",
+  },
+  {
+    name: "JavaScript",
+    tag: "frontend",
+    color: "bg-green-500",
+  },
 
-  export {portlang}
+  {
+    name: "React",
+    tag: "frontend",
+    color: "bg-green-500",
+  },
+  {
+    name: "NextJs",
+    tag: "frontend",
+    color: "bg-green-500",
+  },
+  {
+    name: "AWS",
+    tag: "devops",
+    color: "bg-blue-500",
+  },
+  {
+    name: "Docker",
+    tag: "devops",
+    color: "bg-blue-500",
+  },
+  {
+    name: "Kubernetes",
+    tag: "devops",
+    color: "bg-blue-500",
+  },
+  {
+    name: "DevOps",
+    tag: "devops",
+    color: "bg-blue-500",
+  },
+  {
+    name: "Python",
+    tag: "backend",
+    color: "bg-red-500",
+  },
+
+
+
+  {
+    name: "Express",
+    tag: "backend",
+    color: "bg-red-500",
+  },
+
+  {
+    name: "MySQL",
+    tag: "backend",
+    color: "bg-red-500",
+  },
+];
+
+export { portlang };
